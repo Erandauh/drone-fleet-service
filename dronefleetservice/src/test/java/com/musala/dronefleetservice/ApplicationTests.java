6407a339f141e6e91588eac2b624b0b238e12fe8
Eranda H <erandauh@hotmai.com>
2022-08-23 15:44:14 +0530

    Initial commit of the project

---
+++ dronefleetservice/src/test/java/com/musala/dronefleetservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronefleetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
